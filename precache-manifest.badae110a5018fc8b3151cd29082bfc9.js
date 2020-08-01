65452ba10d3696406246ee52c42e948529332159
sujata <gunalesujata@gmail.com>
2020-08-01 14:14:32 +0530

    Updates

---
+++ precache-manifest.badae110a5018fc8b3151cd29082bfc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63b7e921e2a530244c1424cde80f9438",
+    "url": "/Contact-Manager/index.html"
+  },
+  {
+    "revision": "05f257ff16e6fa8bf40a",
+    "url": "/Contact-Manager/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "da45f8ee0ac0ad334935",
+    "url": "/Contact-Manager/static/css/main.19e5f97a.chunk.css"
+  },
+  {
+    "revision": "05f257ff16e6fa8bf40a",
+    "url": "/Contact-Manager/static/js/2.1887e16f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Contact-Manager/static/js/2.1887e16f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da45f8ee0ac0ad334935",
+    "url": "/Contact-Manager/static/js/main.1948cce5.chunk.js"
+  },
+  {
+    "revision": "7ccb9ef3369148bb365d",
+    "url": "/Contact-Manager/static/js/runtime-main.07445529.js"
+  }
+]);
